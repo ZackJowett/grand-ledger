ddc478b66013b0e7ef9b6a31851f108f154e3c71
ZackJowett <zjwebdev@gmail.com>
2023-05-18 20:04:02 +1000

    implement dynamic api endpoint for fetching a user

---
+++ pages/api/users/[id].js
@@ -0,0 +1,14 @@
+import dbConnect from "/lib/mongodb";
+import User from "/lib/models/user";
+
+export default async function handler(req, res) {
+	await dbConnect();
+
+	try {
+		const user = await User.findOne({ username: req.query.id });
+		res.status(200).json({ success: true, user: user });
+	} catch (error) {
+		console.log("NO USER");
+		res.status(400).json({ success: false });
+	}
+}
