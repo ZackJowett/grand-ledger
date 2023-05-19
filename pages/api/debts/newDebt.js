62e87cf908ba4e41e8df1ba855a20e71a9f98cc9
ZackJowett <zjwebdev@gmail.com>
2023-05-19 10:24:57 +1000

    implemented create new debt api endpoint

---
+++ pages/api/debts/newDebt.js
@@ -0,0 +1,26 @@
+import dbConnect from "/lib/mongodb";
+import Debt from "/lib/models/debt";
+import { useSession } from "next-auth/react";
+
+export default async function handler(req, res) {
+	// Connect to database (connection is likely cached if not first time)
+	await dbConnect();
+
+	try {
+		const debtor = await fetch(
+			`http://localhost:3000/api/users/user1`
+		).then((res) => res.json().then((data) => data.user));
+
+		const debt = await Debt.create({
+			creditor: req.body.userId,
+			debtor: debtor._id,
+			amount: req.body.amount,
+			description: req.body.description,
+		});
+		res.status(201).json({ success: true, data: debt });
+		return;
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ success: false });
+	}
+}
