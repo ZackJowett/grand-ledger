1b261f0ef13aa2a4382519930c94a5401d1b2e45
ZackJowett <zjwebdev@gmail.com>
2023-05-19 10:25:17 +1000

    changed error console log to show thrown error

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 		const user = await User.findOne({ username: req.query.id });
 		res.status(200).json({ success: true, user: user });
 	} catch (error) {
-		console.log("NO USER");
+		console.log("ERROR", error);
 		res.status(400).json({ success: false });
 	}
 }
