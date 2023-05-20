295177f03430888fbedc80c60594c8dbc296d777
zackjowett <zjwebdev@gmail.com>
2023-05-20 16:54:23 +1000

    implemented create settlement handling with mongoose

---
+++ pages/api/settlements/create.js
@@ -0,0 +1,36 @@
+import dbConnect from "/utils/mongodb";
+import Settlement from "/utils/models/settlement";
+import Debt from "/utils/models/debt";
+
+export default async function handler(req, res) {
+	await dbConnect();
+
+	try {
+		// Create new settlement with debts
+		// number increments automatically
+		// Status defaults to pending
+		const settlement = await Settlement.create({
+			settler: req.body.settler,
+			settlee: req.body.settlee,
+			debts: req.body.debts,
+			netAmount: req.body.netAmount,
+			description: req.body.description ? req.body.description : "",
+		});
+
+		// Change status of debts to closed
+		for (let i = 0; i < req.body.debts.length; i++) {
+			const debt = await Debt.findOneAndUpdate(
+				{ _id: req.body.debts[i]._id, closed: false },
+				{ closed: true },
+				{ new: true }
+			);
+
+			console.log(debt);
+		}
+
+		res.status(200).json({ success: true, data: settlement });
+	} catch (error) {
+		console.log("ERROR", error);
+		res.status(400).json({ success: false });
+	}
+}
