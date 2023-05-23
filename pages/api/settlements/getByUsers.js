ed4c5670362ea0a39a9e7a2f8fe0bdd10c4c2c04
zackjowett <zjwebdev@gmail.com>
2023-05-23 17:18:11 +1000

    created getByUser api endpoint for settlements

---
+++ pages/api/settlements/getByUsers.js
@@ -0,0 +1,27 @@
+import dbConnect from "/utils/mongodb";
+import Settlement from "/utils/models/settlement";
+
+export default async function handler(req, res) {
+	await dbConnect();
+
+	try {
+		let settlements = null;
+
+		// Get all settlements with specific user as settler or settlee
+		// Sort by date created in descending order
+		await Settlement.find({
+			$or: [{ settler: req.query.id }, { settlee: req.query.id }],
+		})
+			.sort({ dateCreated: "descending" })
+			.then((res) => {
+				console.log("Result : ", res);
+				settlements = res;
+			})
+			.catch((err) => console.log(err));
+
+		res.status(200).json({ success: true, data: settlements });
+	} catch (error) {
+		console.log("ERROR", error);
+		res.status(400).json({ success: false });
+	}
+}
