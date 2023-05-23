eaa3911006ec0c1f26a604a8b2d4a16f1d2c42a5
zackjowett <zjwebdev@gmail.com>
2023-05-23 18:04:14 +1000

    fixed styles import

--- components/layouts/Layout.jsx
+++ components/layouts/Layout.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
-import styles from "./layout.module.scss";
+import styles from "./Layout.module.scss";
 import Header from "../sections/header/Header";
 
 export default function Layout({ children }) {
