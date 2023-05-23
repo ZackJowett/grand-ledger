62e0e7a7af67eddec24df5d106ef61a3ff989fe9
zackjowett <zjwebdev@gmail.com>
2023-05-23 17:14:09 +1000

    create button elements

---
+++ components/button/Button.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import styles from "./Button.module.scss";
+
+interface Props {
+	className?: string;
+	title: any | React.ReactNode;
+	href?: string;
+	onClick?: Function;
+	dark?: boolean;
+	submit?: boolean;
+	disabled?: boolean;
+}
+
+export default function Button({
+	className,
+	title,
+	href,
+	dark,
+	submit,
+	onClick = null,
+	disabled = false,
+}: Props) {
+	return (
+		<>
+			{href ? (
+				<Link
+					href={href}
+					className={`${className ? className : ""} ${styles.link} ${
+						dark ? styles.dark : ""
+					} ${disabled ? styles.disabled : ""}`}>
+					{title}
+				</Link>
+			) : (
+				<button
+					type={submit ? "submit" : "button"}
+					className={`${className ? className : ""} ${
+						styles.submit
+					} ${dark ? styles.dark : ""} ${
+						disabled ? styles.disabled : ""
+					}`}
+					disabled={disabled}
+					onClick={
+						onClick
+							? () => onClick()
+							: () => {
+									return;
+							  }
+					}>
+					{title}
+				</button>
+			)}
+		</>
+	);
+}
