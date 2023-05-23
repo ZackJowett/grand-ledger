1e673eec9031dc37e018d38510979eb9a53cc666
zackjowett <zjwebdev@gmail.com>
2023-05-23 16:37:54 +1000

    created logo component
    not working with image yet

---
+++ components/logo/Logo.jsx
@@ -0,0 +1,13 @@
+import styles from "./Logo.module.scss";
+import Image from "next/image";
+
+export default function Logo() {
+	return (
+		<Image
+			src="http://localhost:3000/images/logo.png"
+			className={styles.image}
+			height={500}
+			width={500}
+		/>
+	);
+}
