d8c10eb3d633a91faa4eadc3f26f91a665b41860
zackjowett <zjwebdev@gmail.com>
2023-05-23 20:54:43 +1000

    added use state for users

--- components/debt/Debt.jsx
+++ components/debt/Debt.jsx
@@ -3,8 +3,12 @@ import ClickableCard from "/components/card/ClickableCard";
 import TextWithTitle from "/components/text/title/TextWithTitle";
 import { getName, formatDate } from "/utils/helpers";
 import Money from "/components/text/money/Money";
+import { useStore } from "react-redux";
 
-export default function Debt({ debt, globals }) {
+export default function Debt({ debt, session, className }) {
+	const state = useStore().getState();
+	const users = state.userList.users;
+
 	const shortDesc =
 		debt.description.length > 25
 			? debt.description.slice(0, 25) + "..."
@@ -12,10 +16,11 @@ export default function Debt({ debt, globals }) {
 
 	return (
 		<ClickableCard
-			title={getName(debt.creditor, globals.users, globals.session)}
+			title={getName(debt.creditor, users, session)}
 			pretitle={debt.closed ? "I owed" : "I owe"}
 			badge={debt.closed ? "Closed" : "Open"}
-			href={`/debts/${debt._id}`}>
+			href={`/debts/${debt._id}`}
+			className={`${className ? className : ""}`}>
 			<TextWithTitle
 				title={<Money amount={debt.amount} />}
 				text="Amount"
