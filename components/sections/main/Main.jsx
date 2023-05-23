d1cae6e4372b2ea1e5e50c08f8537aff8f1ffc9d
zackjowett <zjwebdev@gmail.com>
2023-05-23 17:34:55 +1000

    fixed imports

--- components/sections/main/Main.jsx
+++ components/sections/main/Main.jsx
@@ -1,9 +1,9 @@
 import styles from "./Main.module.scss";
 import { useSession } from "next-auth/react";
 import Button from "../../button/Button";
-import NetPosition from "components/sections/stats/NetPosition";
-import RecentSettlements from "components/sections/recents/RecentSettlements";
-import RecentDebts from "components/sections/recents/RecentDebts";
+import NetPosition from "../../sections/stats/NetPosition";
+import RecentSettlements from "../../sections/recents/RecentSettlements";
+import RecentDebts from "../../sections/recents/RecentDebts";
 import LoggedOut from "../login/loggedOut/LoggedOut";
 
 export default function Main() {
