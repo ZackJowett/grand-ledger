b57dc5e1780801f4192b39234e81d6e66af08a89
zackjowett <zjwebdev@gmail.com>
2023-05-24 20:11:32 +1000

    added background and small options to attributes

--- components/text/money/Money.jsx
+++ components/text/money/Money.jsx
@@ -1,8 +1,17 @@
 import styles from "./Money.module.scss";
 
-export default function Money({ amount, className, includeSign = false }) {
+export default function Money({
+	amount,
+	className,
+	includeSign = false,
+	background = false,
+	small = false,
+}) {
 	return (
-		<span className={`${className ? className : ""} ${styles.money}`}>
+		<span
+			className={`${className ? className : ""} ${styles.money} ${
+				background ? styles.background : ""
+			} ${small ? styles.small : ""} `}>
 			<span className={styles.sign}>$</span>{" "}
 			{includeSign ? (amount < 0 ? "-" : "+") : ""}
 			{Math.abs(amount).toFixed(2).toLocaleString("en-US")}
