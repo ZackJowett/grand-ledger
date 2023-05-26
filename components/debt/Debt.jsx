fe8a9d297162d0a4b74a54c02964e342c3694958
zackjowett <zjwebdev@gmail.com>
2023-05-26 20:34:25 +1000

    updated with new "status" options

--- components/debt/Debt.jsx
+++ components/debt/Debt.jsx
@@ -6,41 +6,59 @@ import Money from "/components/text/money/Money";
 import { useStore } from "react-redux";
 import { useSession } from "next-auth/react";
 
-export default function Debt({ debt, className }) {
+export default function Debt({ debt, className, unreceived = false }) {
 	const { data: session } = useSession();
 	const state = useStore().getState();
 	const users = state.userList.users;
 
-	const shortDesc =
-		debt.description.length > 25
-			? debt.description.slice(0, 25) + "..."
-			: debt.description;
+	// Get who has a debt with logged in user
+	let debtWith = "";
+	if (debt.debtor == session.user.id) {
+		// User is debtor
+		debtWith = getName(debt.creditor, users, session);
+	} else {
+		// User is cerditor
+		debtWith = getName(debt.debtor, users, session);
+	}
 
 	return (
 		<ClickableCard
-			title={getName(debt.creditor, users, session)}
-			pretitle={debt.closed ? "I owed" : "I owe"}
-			badge={debt.closed ? "Closed" : "Open"}
+			title={debtWith}
+			pretitle="Debt"
+			badge={getStatus(debt.status, unreceived)}
 			href={`/debts/${debt._id}`}
 			className={`${className ? className : ""}`}>
 			<TextWithTitle
 				pretitle="Debt"
 				title={<Money amount={debt.amount} />}
-				text="Amount"
+				text={
+					debt.debtor == session.user.id
+						? debt.status == "closed"
+							? "You owed"
+							: `You owe`
+						: debt.status == "closed"
+						? `${debtWith} owed me`
+						: `${debtWith} owes me`
+				}
 				align="left"
 				reverse
 				className={styles.amount}
 			/>
-			{/* <TextWithTitle
-				title={shortDesc}
-				text="Description"
-				align="left"
-				reverse
-				small
-				className={styles.desc}
-			/> */}
-
 			<p className={styles.date}>Opened {formatDate(debt.dateCreated)}</p>
 		</ClickableCard>
 	);
+}
+
+function getStatus(debtStatus, unreceived) {
+	if (debtStatus == "closed") {
+		return "Closed";
+	} else if (debtStatus == "outstanding") {
+		// Return "Unreceived" if the debt is viewed as an unreceived payment
+		// otherwise just return "Outstanding"
+		return unreceived ? "Unreceived" : "Outstanding";
+	} else if (debtStatus == "pending") {
+		return "Pending";
+	}
+	console.log("ERROR getting debt status");
+	return "";
 }
