91b7324de215f3cdd0788607c9db415cb0abfe9b
zackjowett <zjwebdev@gmail.com>
2023-05-26 20:34:49 +1000

    formatted properly with cards

--- pages/debts/index.jsx
+++ pages/debts/index.jsx
@@ -3,9 +3,16 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { getAllForDebtor } from "/utils/data/debts";
 import { getAllUsers } from "/utils/data/users";
-import Debt from "/components/debt/Debt";
-import Layout from "../../components/layouts/Layout";
-import LoggedOut from "../../components/sections/login/loggedOut/LoggedOut";
+import Debt from "components/debt/Debt";
+import Layout from "components/layouts/Layout";
+import LoggedOut from "components/sections/login/loggedOut/LoggedOut";
+import styles from "../../public/styles/pages/Debts.module.scss";
+import Button from "components/button/Button";
+import Card from "components/card/Card";
+import TextWithTitle from "components/text/title/TextWithTitle";
+import { filterDebts } from "/utils/helpers";
+import Money from "components/text/money/Money";
+import { CardPlaceholder } from "components/placeholders/Placeholders";
 
 export default function Debts() {
 	const { data: session } = useSession();
@@ -15,8 +22,8 @@ export default function Debts() {
 	// const [openDebts, setOpenDebts] = useState(null); // debts.filter(debt => !debt.closed)
 	// const [closedDebts, setClosedDebts] = useState(null);
 	const [users, setUsers] = useState(null);
-	const [filter, setFilter] = useState("open"); // "all" || "open" || "closed"
-	const [totalUnreceived, setTotalUnreceived] = useState(null);
+	const [filter, setFilter] = useState("outstanding"); // "all" || "open" || "closed"
+	const [totalDebt, setTotalDebt] = useState(null);
 
 	// Get all debts associated with logged in user
 	useEffect(() => {
@@ -37,9 +44,9 @@ export default function Debts() {
 
 		let total = 0;
 
-		// Get total amount unreceived
+		// Get total amount outstanding
 		debts.forEach((debt) => {
-			if (!debt.closed) {
+			if (debt.status == "outstanding") {
 				total += debt.amount;
 			}
 		});
@@ -47,7 +54,7 @@ export default function Debts() {
 		// setOpenDebts(debts.filter((debt) => !debt.closed));
 		// setClosedDebts(debts.filter((debt) => debt.closed));
 
-		setTotalUnreceived(total.toFixed(2));
+		setTotalDebt(total.toFixed(2));
 	}, [debts]);
 
 	// Not logged in
@@ -62,55 +69,78 @@ export default function Debts() {
 	// Filter debts
 	const handleFilterSelect = (e) => {
 		setFilter(e.target.value);
-	};
-
-	const filterDebt = (debt) => {
-		if (!filter) return true;
-		if (filter == "all") return true;
-		if (filter == "open" && !debt.closed) return true;
-		if (filter == "closed" && debt.closed) return true;
+		console.log(filter);
 	};
 
 	// Logged in
 	return (
 		<Layout>
-			<h1>Debts</h1>
-			<p>A debt is the amount of money you owe someone</p>
-			<p>
-				You can settle multiple debts to the same person at once{" "}
-				<Link href="/settlements/create">here</Link>
-			</p>
-			<br />
-			<Link href="/debts/create">New Debt</Link>
-			<br />
-			<h3>
-				Total Amount (Open):{" $"}
-				{totalUnreceived !== null ? totalUnreceived : "Calculating..."}
-			</h3>
+			<section className={styles.wrapper}>
+				<Card dark>
+					<TextWithTitle
+						title="Debts"
+						text="A debt is the amount of money you owe someone"
+						align="left"
+					/>
+					<br />
+					<p>
+						You can settle multiple debts to the same person at once{" "}
+						<Link
+							href="/settlements/create"
+							className={styles.link}>
+							here
+						</Link>
+					</p>
+				</Card>
+				<Card dark>
+					<h3>
+						Total Amount (Outstanding):
+						{totalDebt !== null ? (
+							<Money amount={totalDebt} />
+						) : (
+							"Calculating..."
+						)}
+					</h3>
+				</Card>
+				<Button
+					title="Create Debt"
+					href="/debts/create"
+					className={styles.create}
+				/>
+				<Card dark title="Debts" className={styles.debtsWrapper}>
+					<hr className={styles.hr} />
+					<select onChange={handleFilterSelect}>
+						<option value="outstanding">Outstanding</option>
+						<option value="pending">Pending</option>
+						<option value="closed">Closed</option>
+						<option value="all">All</option>
+					</select>
+					<div className={styles.cards}>
+						{debts ? (
+							debts.map((debt, index) => {
+								if (!filterDebts(debt, filter)) return;
 
-			{/* Select Debt filter */}
-			<select onChange={handleFilterSelect}>
-				<option value="open">Open</option>
-				<option value="closed">Closed</option>
-				<option value="all">All</option>
-			</select>
-
-			{debts ? (
-				debts.map((debt, index) => {
-					// Filter debts
-					if (!filterDebt(debt)) return;
-
-					return (
-						<Debt
-							key={index}
-							debt={debt}
-							globals={{ session: session, users: users }}
-						/>
-					);
-				})
-			) : (
-				<p>Loading...</p>
-			)}
+								return (
+									<Debt
+										key={index}
+										debt={debt}
+										globals={{
+											session: session,
+											users: users,
+										}}
+									/>
+								);
+							})
+						) : (
+							<>
+								<CardPlaceholder />
+								<CardPlaceholder />
+								<CardPlaceholder />
+							</>
+						)}
+					</div>
+				</Card>
+			</section>
 		</Layout>
 	);
 }
