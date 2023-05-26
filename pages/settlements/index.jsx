f64b40bf23458b44d2d531e44ced67284ef6362f
zackjowett <zjwebdev@gmail.com>
2023-05-26 20:35:26 +1000

    implemented card layout

--- pages/settlements/index.jsx
+++ pages/settlements/index.jsx
@@ -3,11 +3,15 @@ import { useEffect, useState } from "react";
 import { getAllSettlements } from "/utils/data/settlements";
 import Settlement from "/components/settlement/Settlement";
 import Button from "/components/button/Button";
-import styles from "public/styles/pages/Settlements.module.scss";
+import styles from "public/styles/pages/Debts.module.scss";
 import Layout from "/components/layouts/Layout";
 import LoggedOut from "/components/sections/login/loggedOut/LoggedOut";
 import Card from "/components/card/Card";
 import { useStore } from "react-redux";
+import TextWithTitle from "components/text/title/TextWithTitle";
+import Link from "next/link";
+import { filterSettlements } from "/utils/helpers";
+import { CardPlaceholder } from "components/placeholders/Placeholders";
 
 export default function Settlements() {
 	// Session
@@ -19,6 +23,7 @@ export default function Settlements() {
 	// User logged in
 	// States
 	const [settlements, setSettlements] = useState(null);
+	const [filter, setFilter] = useState("pending"); // "all" || "open" || "closed"
 
 	// Get settlements
 	useEffect(() => {
@@ -38,6 +43,10 @@ export default function Settlements() {
 		);
 	}
 
+	const handleFilterSelect = (e) => {
+		setFilter(e.target.value);
+	};
+
 	return (
 		<Layout>
 			{/* <InfoBanner title="What is a settlement?">
@@ -56,29 +65,67 @@ export default function Settlements() {
 					person approves it.
 				</p>
 			</InfoBanner> */}
+			<section className={styles.wrapper}>
+				<Card dark>
+					<TextWithTitle
+						title="Settlements"
+						text="An offer to close multiple debts to the same person"
+						align="left"
+					/>
+					<br />
+					<p>
+						You can settle multiple debts to the same person at once{" "}
+						<Link
+							href="/settlements/create"
+							className={styles.link}>
+							here
+						</Link>
+					</p>
+				</Card>
+				<Card dark>
+					<h3>Totals</h3>
+				</Card>
+				<Button
+					title="Create Settlement"
+					href="/settlements/create"
+					className={styles.create}
+				/>
+				<Card dark title="Settlements">
+					<hr />
+					<select onChange={handleFilterSelect}>
+						<option value="pending">Pending</option>
+						<option value="reopened">Reopened</option>
+						<option value="closed">Closed</option>
+						<option value="all">All</option>
+					</select>
+					<div className={styles.cards}>
+						{settlements ? (
+							settlements.map((settlement, index) => {
+								if (!filterSettlements(settlement, filter))
+									return;
 
-			<Button
-				title="Create Settlement"
-				href="/settlements/create"
-				className={styles.newSettlement}
-			/>
-			<Card dark title="Settlements" className={styles.settlements}>
-				{settlements
-					? settlements.map((settlement, index) => {
-							return (
-								<Settlement
-									key={index}
-									settlement={settlement}
-									globals={{
-										users: state.userList.users,
-										session: session,
-									}}
-									className={styles.settlement}
-								/>
-							);
-					  })
-					: "Loading settlements..."}
-			</Card>
+								return (
+									<Settlement
+										key={index}
+										settlement={settlement}
+										globals={{
+											users: state.userList.users,
+											session: session,
+										}}
+										className={styles.settlement}
+									/>
+								);
+							})
+						) : (
+							<>
+								<CardPlaceholder />
+								<CardPlaceholder />
+								<CardPlaceholder />
+							</>
+						)}
+					</div>
+				</Card>
+			</section>
 		</Layout>
 	);
 }
