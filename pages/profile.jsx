a974f464edb183d52a81afd8607843cd5bbbf8ac
ZackJowett <zjwebdev@gmail.com>
2023-05-28 18:18:19 +1000

    added yet-to-be-completed components and starter info

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,23 +1,39 @@
 import { signOut, useSession } from "next-auth/react";
 import Layout from "../components/layouts/Layout";
 import LoggedOut from "../components/sections/login/loggedOut/LoggedOut";
+import Card from "../components/card/Card";
+import styles from "public/styles/pages/Profile.module.scss";
+import Button from "../components/button/Button";
 
 export default function Profile() {
 	const { data: session } = useSession();
 
 	return (
 		<Layout>
-			{session ? (
-				<>
-					<p>You are logged in</p>
-					<p>Name: {session.user.name}</p>
-					<p>Email: {session.user.email}</p>
-					<p>Username: {session.user.username}</p>
-					<button onClick={() => signOut()}>Sign out</button>
-				</>
-			) : (
-				<LoggedOut />
-			)}
+			<section className={styles.section}>
+				{session ? (
+					<>
+						{/* <ProfilePhoto /> */}
+						<Card pretitle="Hello" title={session.user.name} dark />
+						{/* <UserDetails /> */}
+						{/* <UserBankDetails /> */}
+						{/* <UserStatistics /> */}
+
+						<Card dark>
+							<p>You are logged in</p>
+							<p>Name: {session.user.name}</p>
+							<p>Email: {session.user.email}</p>
+							<p>Username: {session.user.username}</p>
+							<Button
+								title="Sign out"
+								onClick={() => signOut()}
+							/>
+						</Card>
+					</>
+				) : (
+					<LoggedOut />
+				)}
+			</section>
 		</Layout>
 	);
 }
