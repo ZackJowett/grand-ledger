a5bd4c4c59dd7df5f6ea4765a5c4be287ca3d046
ZackJowett <zjwebdev@gmail.com>
2023-05-30 23:08:02 +1000

    partially implemented debt details page

--- pages/debts/[debt].jsx
+++ pages/debts/[debt].jsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { getAllDebts, getOneDebt } from "/utils/data/debts";
-import Layout from "../../components/layouts/Layout";
-import LoggedOut from "../../components/sections/login/loggedOut/LoggedOut";
+import Layout from "components/layouts/Layout";
+import LoggedOut from "components/sections/login/loggedOut/LoggedOut";
+import Card from "components/card/Card";
+import styles from "public/styles/pages/Debt.module.scss";
+import TextWithTitle from "components/text/title/TextWithTitle";
+import { getName } from "utils/helpers";
+import Badge from "components/text/badge/Badge";
+import { useStore } from "react-redux";
+import Money from "components/text/money/Money";
 
 // Get all debts associated with logged in user and export as path options
 // for next.js router
@@ -35,6 +42,7 @@ export async function getStaticProps({ params }) {
 
 export default function Debt({ debt }) {
 	const { data: session } = useSession();
+	const state = useStore().getState();
 
 	// Not logged in
 	if (!session) {
@@ -45,20 +53,65 @@ export default function Debt({ debt }) {
 		);
 	}
 
+	// Get name of other party (not logged in user)
+	const otherParty =
+		debt.debtor == session.user.id ? debt.creditor : debt.debtor;
+	const otherPartyName = getName(otherParty, state.userList.users, session);
+
+	// Get status of debt
+	const status =
+		debt.status == "pending"
+			? "Pending"
+			: debt.status == "closed"
+			? "Closed"
+			: "Outstanding";
+
 	if (!debt) return null;
 	return (
 		<Layout>
-			<h1>Debt</h1>
-			<Link href="/">Home</Link>
-			<hr />
-			<div>
-				<p>Creditor {debt.creditor}</p>
-				<p>Debtor: {debt.debtor}</p>
-				<p>Amount: ${debt.amount}</p>
-				<p>Description: {debt.description}</p>
-				<p>Status: {debt.closed ? "Closed" : "Open"}</p>
-				<p>ID: {debt._id}</p>
-			</div>
+			<section className={styles.wrapper}>
+				<div className={styles.header}>
+					<TextWithTitle
+						title={`Debt with ${otherPartyName}`}
+						text={`Identifier: ${debt._id}`}
+						className={styles.title}
+						align="left"
+						large
+					/>
+					<Badge
+						title={status}
+						color={debt.status}
+						className={styles.badge}
+					/>
+				</div>
+
+				<Card title="Amount" dark>
+					<TextWithTitle
+						title={`Total owed to ${otherPartyName}`}
+						text={
+							<Money
+								amount={debt.amount}
+								className={styles.debt}
+								background
+								small
+							/>
+						}
+						className={styles.textWithTitle}
+						align="left"
+						tiny
+					/>
+
+					<div>
+						<p>Creditor {debt.creditor}</p>
+						<p>Debtor: {debt.debtor}</p>
+						<p>Amount: ${debt.amount}</p>
+						<p>Description: {debt.description}</p>
+						<p>Status: {debt.closed ? "Closed" : "Open"}</p>
+						<p>ID: {debt._id}</p>
+					</div>
+				</Card>
+				<hr />
+			</section>
 		</Layout>
 	);
 }
