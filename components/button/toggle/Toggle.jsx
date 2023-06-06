4db1578d40c8bf7b1812d355eed1761a840da21d
ZackJowett <zjwebdev@gmail.com>
2023-06-06 18:52:15 +1000

    added className attribute

--- components/button/toggle/Toggle.jsx
+++ components/button/toggle/Toggle.jsx
@@ -1,9 +1,17 @@
 import styles from "./Toggle.module.scss";
 
-export default function Toggle({ title, active = false, icon, onClick }) {
+export default function Toggle({
+	title,
+	active = false,
+	icon,
+	onClick,
+	className,
+}) {
 	return (
 		<div
-			className={`${styles.wrapper} ${active ? styles.active : ""}`}
+			className={`${styles.wrapper} ${active ? styles.active : ""} ${
+				className ? className : ""
+			}`}
 			onClick={onClick}>
 			<div className={styles.icon}>{icon}</div>
 			{title}
