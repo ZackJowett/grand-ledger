acb7db970f8208bca95cf8fc4365daf5301eba16
ZackJowett <zjwebdev@gmail.com>
2023-06-23 19:17:53 +1000

    added loading display when session loading

--- components/sections/login/loggedOut/LoggedOut.jsx
+++ components/sections/login/loggedOut/LoggedOut.jsx
@@ -3,8 +3,22 @@ import TextWithTitle from "/components/text/title/TextWithTitle";
 import Button from "/components/button/Button";
 import styles from "./LoggedOut.module.scss";
 import FullScreen from "/components/layouts/FullScreen";
+import { useSession } from "next-auth/react";
+import Spinner from "/components/placeholders/spinner/Spinner";
+import Layout from "/components/layouts/Layout";
 
 export default function LoggedOut() {
+	const { data: session, status: sessionStatus } = useSession();
+
+	// User not logged in
+	if (sessionStatus == "loading") {
+		return (
+			<Layout>
+				<Spinner />
+			</Layout>
+		);
+	}
+
 	return (
 		<FullScreen>
 			<Card className={styles.wrapper}>
