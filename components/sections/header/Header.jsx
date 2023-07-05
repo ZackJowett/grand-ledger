f4ffa0b93e5b1fb0bada1b811a0654b7bfbafe77
ZackJowett <zjwebdev@gmail.com>
2023-07-05 11:00:55 +1000

    added logo

--- components/sections/header/Header.jsx
+++ components/sections/header/Header.jsx
@@ -1,13 +1,15 @@
 import MobileNav from "../nav/MobileNav";
 import styles from "./Header.module.scss";
 import Link from "next/link";
+import Logo from "components/logo/Logo";
 
 export default function Header({ currentRoute }) {
 	return (
 		<header className={styles.header}>
 			<div className={styles.floatingHeader}>
-				<Link href="/" className={styles.title}>
-					<h1>Grand Ledger</h1>
+				<Link href="/" className={styles.titleWrapper}>
+					<Logo />
+					<h1 className={styles.title}>Grand Ledger</h1>
 				</Link>
 				<MobileNav currentRoute={currentRoute} />
 			</div>
